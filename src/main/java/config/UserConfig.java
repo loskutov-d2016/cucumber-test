7a17a1c8ba3a13650e9e9178ff6ed22cb9e8ec5b
Лоскутов Дмитрий Сергеевич <dmitry.loskutov@russianpost.ru>
2021-10-01 10:28:17 +0300

    1 main

---
+++ src/main/java/config/UserConfig.java
@@ -0,0 +1,7 @@
+package config;
+
+public class UserConfig {
+    public static final String USER_LOGIN = "loskutov-d@list.ru";
+    public static final String USER_PASSWORD = "8803698398CVBzxc";
+}
+
---
+++ src/main/java/pages/SignInPage.java
@@ -0,0 +1,20 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class SignInPage {
+    private SelenideElement loginInput = $(By.xpath("//input[@id=\"email\"]"));
+    private SelenideElement passwordInput = $(By.xpath("//input[@id=\"password\"]"));
+
+    public void inputLogin(String text) {
+        this.loginInput.val(text);
+    }
+
+    public void inputPassword(String text) {
+        this.passwordInput.val(text);
+    }
+
+}
